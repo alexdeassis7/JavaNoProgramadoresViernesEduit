5845c2f63ce996afffe9dce951fe7bf5be282157
Alex <alexdeassis7@gmail.com>
2020-06-05 20:29:03 -0300

    Clase 4 Array y repaso de clase 3

---
+++ Clase4ArraysUnidimencionales/src/clase4arraysunidimencionales/Clase4ArraysUnidimencionales.java
@@ -0,0 +1,23 @@
+package clase4arraysunidimencionales;
+//Calcular la temperatura promedio del mes de agosto 
+
+import java.util.Scanner;
+
+public class Clase4ArraysUnidimencionales {
+
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        int numero = 90 ;//defino e inicializo 
+        //int numero ; //defino un espacio en memoria que podra contener un dato INTEGER
+        //numero = 90 ; //incializo la variable con el valor 90
+       
+        
+        float[] temperaturas = new float[31];
+        
+        temperaturas[3] = 45; 
+
+        System.out.println("ingrese tem del dia 1 ");
+
+    }
+
+}
