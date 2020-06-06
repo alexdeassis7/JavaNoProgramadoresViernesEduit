5bd3434b92928875e90c5f1c9ed9f9cd82662fd3
Alex <alexdeassis7@gmail.com>
2020-06-05 22:16:03 -0300

    Clase4

---
+++ Clasee4/src/clase4/Clase4.java
@@ -0,0 +1,25 @@
+package clase4;
+
+public class Clase4 {
+
+    public static void main(String[] args) {
+        
+        int diasAgosto = 31;
+        float[] temperaturas = new float[diasAgosto];
+        
+        
+        for (int i = 0; i < diasAgosto; i++) {
+            System.out.println("ingrese la temp del dia " + (i + 1));
+        }
+        
+//        int[] edades = {25, 26, 58, 96, 21, 14}; //creamos y asignamos valores a un array de enteros 
+//        System.out.println("Comenzamos a imprimir los valores del array");
+//
+//        for (int indice = 0; indice <= 6; indice++) {
+//            System.out.println("edades [" + indice + "]=" + edades[indice]);
+//        }
+//        System.out.println("Finalizamos imprimir los valores del array");
+
+    }
+
+}
